fc379c40d67b3c131c293c6e1cf0277a5f8ca3bc
oanhtran020906-sys <oanhtran020906@gmail.com>
2026-01-04 21:43:04 +0700

    Update data_loader.py

--- ecg-mitbih-analysis/src/data_loader.py
+++ ecg-mitbih-analysis/src/data_loader.py
@@ -1,11 +1,18 @@
-# Tải dữ liệu WFDB
 import wfdb
 import pandas as pd
 import numpy as np
 
-record = wfdb.rdrecord("100", pn_dir="mitdb")
-signal = record.p_signal[:, 0]  # first channel
-fs = record.fs
 
-times = pd.to_timedelta(np.arange(len(signal)) / fs, unit="s")
-ecg_series = pd.Series(signal, index=times)
+def load_ecg_record(record_id="100", channel=0, db_name="mitdb"):
+    """
+    Load ECG record from MIT-BIH database using WFDB
+    """
+
+    record = wfdb.rdrecord(record_id, pn_dir=db_name)
+    signal = record.p_signal[:, channel]
+    fs = record.fs
+
+    times = pd.to_timedelta(np.arange(len(signal)) / fs, unit="s")
+    ecg_series = pd.Series(signal, index=times)
+
+    return ecg_series, fs
